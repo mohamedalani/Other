679c91d89fb1f15fc1a386db2e6f92d5d97c61ed
Al Ani, Mohamed <mohamed.alani@ext.ercom.fr>
2017-11-24 19:10:17 +0100

    add

---
+++ Challenge DigitalGenius/part1.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+if __name__ == '__main__':
+	print("extracting messages, please wait...")
+	path = "../../../data/enron-email-dataset/"
+
+	df = pd.read_csv(path+"emails.csv")
+
+	df["date"] = df["message"].map(lambda x : x.split("\n")[1][5:])
+	df["sender"] = df["message"].map(lambda x : x.split("\n")[2][5:].strip())
+	df["receiver"] = df["message"].map(lambda x : x.split("\n")[3][3:].strip())
+	df["subject"] = df["message"].map(lambda x : x.split("\n")[4][7:].strip())
+	df["content"] = df["message"].map(lambda x : " ".join(x.split("X-")[-1].split("\n")[1:]))
+	df["date"] = pd.to_datetime(df["date"])
+	df["time"] = df["date"].map(lambda x : x.time())
+	df["date"] = df["date"].map(lambda x : x.date())
+
+	df.drop(["message", "file"], axis=1, inplace=True)
+
+	df.to_csv(path+"emails_clean.csv")
+	print("Successful Extraction, Please run part2.py for the API")
+	print(df.head())
---
+++ Challenge DigitalGenius/part2.py
@@ -0,0 +1,88 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+import csv, sqlite3
+from dateutil.parser import parse
+import re
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+#Path to where the csv is
+path = "../../../data/enron-email-dataset/"
+
+#reading csv
+df = pd.read_csv(path+"emails_clean.csv")
+
+#Creating the database
+con = sqlite3.connect("test.sqlite")
+db_connect = create_engine('sqlite:///test.sqlite')
+try:
+    df.to_sql("emails_clean", db_connect,  if_exists='append', index=False)
+except: 
+    pass
+
+#Creating the API
+app = Flask(__name__)
+api = Api(app)
+ 
+#Filter by sender Address
+class sender(Resource):
+    def get(self, sender_mail):
+        if not re.match("[^@]+@[^@]+\.[^@]+", sender_mail):
+            return("Not a valid email address. Please try again.")
+        
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select date, time, sender, receiver, subject, content from emails_clean where sender = '%s' " %sender_mail  ) # This line performs query and returns json result
+        result = {'mails found': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+#Filter by Recipient Addresses
+class recipient(Resource):
+    def get(self, recipient_mails):
+        conn = db_connect.connect() # connect to database
+        result = {}
+        mails = recipient_mails.split(",")
+		
+		#Checking if it has a email syntax
+        for mail in mails:
+            if not re.match("[^@]+@[^@]+\.[^@]+", mail):
+                return("Not a valid email address. Please try again : xx@xx.xx, xx@xx.xx")
+          
+        for mail in mails:
+            query = conn.execute("select date, time, sender, receiver, subject, content from emails_clean where receiver LIKE '%"+mail+"%'") # This line performs query and returns json result
+            result[mail] = [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
+        return jsonify({"mails found" : result})
+
+#Filter by date range
+class date_range(Resource):
+    def get(self, range_):
+        conn = db_connect.connect() # connect to database
+        dates = range_.split(",")
+        
+        #Check if we have 2 dates 
+        if len(dates) != 2:
+            return("not a valid date interval, please enter a valid date interval : yyyy-mm-dd,yyyy-mm-dd")
+        
+        #Check if a valid date format
+        for date in dates:
+            try:
+                parse(date)
+            except: 
+                return("not a valid date interval, please enter a valid date interval : yyyy-mm-dd,yyyy-mm-dd")
+        
+        query = conn.execute("select date, time, sender, receiver, subject, content from emails_clean where date BETWEEN '"+dates[0]+"' and '"+dates[1]+"'") # This line performs query and returns json result
+        result  = {"mails found" : [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+#Adding routes
+api.add_resource(sender, '/sender/<sender_mail>') # Route_1
+api.add_resource(recipient, '/recipient/<recipient_mails>') # Route_2
+api.add_resource(date_range, '/date_range/<range_>') # Route_2
+
+
+
+if __name__ == '__main__':
+     app.run(port='5002')
